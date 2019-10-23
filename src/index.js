705a0f0e70298ba4b20e1305802a7ab54ed59f16
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2019-10-23 09:15:53 -0300

    Revert "my changes"
    This reverts commit 4624b08e9f680df18db0573ae869da8d62c7c9da.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-
 
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-    <Provider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
     , document.getElementById("root"));
