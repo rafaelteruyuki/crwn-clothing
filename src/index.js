e2aadb518d81f69fba093737c47a4f4246692dfd
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2019-10-23 09:52:34 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
@@ -10,9 +9,10 @@ import App from "./App";
 import store from "./redux/store";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
-    , document.getElementById("root"));
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
