ddd803da2394e6a62c57067f30b58c39bb17e81a
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2019-10-23 09:08:53 -0300

    Revert "marge"
    This reverts commit 11d387d83385624ea0a336766d8fb21d77592192.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import App from "./App";
 
 ReactDOM.render(
 <<<<<<< HEAD
-<<<<<<< HEAD
   <BrowserRouter>
     <App />
   </BrowserRouter>
@@ -20,9 +19,3 @@ ReactDOM.render(
     </Provider>
     , document.getElementById("root"));
 >>>>>>> parent of 094d35d... Merge branch 'master' of https://github.com/rafaelteruyuki/crwn-clothing
-=======
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    , document.getElementById("root"));
->>>>>>> parent of f66d887... first files to implement redux on user
