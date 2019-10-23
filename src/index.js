98b93b66666500c4e2bf2294ede8d3b80f5bf264
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2019-10-23 09:17:20 -0300

    undo merge

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
+<<<<<<< HEAD
     <BrowserRouter>
         <App />
     </BrowserRouter>
+=======
+    <Provider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+>>>>>>> parent of 094d35d... Merge branch 'master' of https://github.com/rafaelteruyuki/crwn-clothing
     , document.getElementById("root"));
