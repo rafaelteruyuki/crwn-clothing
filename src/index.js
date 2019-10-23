3e64ce8861fa0c6023112e9a51b3953da45d8c32
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2019-10-23 09:14:55 -0300

    undo merge

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 
 import "./index.css";
 import App from "./App";
+
+import store from "./redux/store";
 
 ReactDOM.render(
   <Provider>
