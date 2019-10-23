34b0098f4e200cb59523f723471683769a9906be
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2019-10-23 09:24:51 -0300

    Revert "Merge branch 'master' of https://github.com/rafaelteruyuki/crwn-clothing"
    This reverts commit 094d35d22ff131494d2f7a6d142547051910247a, reversing
    changes made to 4624b08e9f680df18db0573ae869da8d62c7c9da.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { connect } from "react-redux";
 
 import "./App.css";
 
@@ -10,25 +9,32 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
-import { setCurrentUser } from "./redux/user/user.actions";
-
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapshot => {
-          setCurrentUser({
-            id: snapshot.id,
-            ...snapshot.data()
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
           });
         });
       } else {
-        setCurrentUser(userAuth);
+        this.setState({ currentUser: userAuth });
       }
     });
   }
@@ -40,7 +46,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -51,11 +57,4 @@ class App extends React.Component {
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(App);
+export default App;
