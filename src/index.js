b18739feac4af075dc819909c32ac3e6262276d1
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2019-10-23 09:10:34 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,16 +6,9 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-<<<<<<< HEAD
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
   , document.getElementById("root"));
-=======
-    <Provider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
-    , document.getElementById("root"));
->>>>>>> parent of 094d35d... Merge branch 'master' of https://github.com/rafaelteruyuki/crwn-clothing
